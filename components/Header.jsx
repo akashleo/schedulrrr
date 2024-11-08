ed8b60a516b216d7bbee0ab265249ff1cd0c75a6
Akash Ghosh <akashleo2009@gmail.com>
2024-11-08 23:32:19 +0530

    userbutton menu addition

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,10 +3,12 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
 import { PenBox } from "lucide-react";
+import { SignedOut, SignedIn, UserButton, SignInButton } from "@clerk/nextjs";
 
 const Header = () => {
   return (
     <nav className="mx-auto px-4 py-2 flex justify-between">
+      <div>
       <Link href="/home" className="flex items-center">
         <Image
           src="/logo.png"
@@ -16,11 +18,22 @@ const Header = () => {
           className="h-16 w-auto"
         />
       </Link>
-      <div>
+      </div>
+      <div className="flex flex-row items-center gap-4">
         <Link href="/events?create=true">
-        <Button className="flex items-center gap-2"><PenBox size={18}/> Create Workflow</Button>
+          <Button className="flex items-center gap-2">
+            <PenBox size={18} /> 
+             <span className="hidden sm:inline">Create Event</span>
+          </Button>
         </Link>
-        <Button variant="outline">Log In</Button>
+        <SignedOut className="inline-block">
+          <SignInButton forceRedirectUrl="/dashboard">
+            <Button variant="outline">Log In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn className="inline-block">
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
